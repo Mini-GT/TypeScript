810a6cb4a3fa2c391a1fbedbeaac8765fd21a600
MiniGT <bendanillo539@gmail.com>
2024-08-23 10:57:04 +0800

    recommit because some files cannot be auto staged so i have to do it manually

---
+++ LiteralTypes/literalTypes.ts
@@ -0,0 +1,3 @@
+let myName = "Bob";
+// myName2 is literally named "Bob" since it is a const that cannot be changed
+const myName2: "Bob" = "Bob";
---
+++ ObligatoryTypes/index.ts
@@ -0,0 +1,3 @@
+// let myName: string = "bob";
+let numberOfWheels: number = 4;
+let isStudent: boolean = false;

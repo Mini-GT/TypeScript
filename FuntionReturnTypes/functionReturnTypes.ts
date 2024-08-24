c66fe83d6b30426d09f51b1e3e9567137e4d288e
MiniGT <bendanillo539@gmail.com>
2024-08-24 09:44:31 +0800

    learned [: void] return type and added all utility functions with a return type

--- FuntionReturnTypes/functionReturnTypes.ts
+++ FuntionReturnTypes/functionReturnTypes.ts
@@ -1,8 +1,8 @@
-type UserRole = "guest" | "member" | "admin"
+type UserRole = "guest" | "member" | "admin";
 
 type User = {
-    username: string
-    role: UserRole
+    username: string;
+    role: UserRole;
 }
 
 const users: User[] = [
@@ -11,10 +11,13 @@ const users: User[] = [
     { username: "guest_user", role: "guest" }
 ];
 
-function fetchUserDetails(username: string) {
-    const user = users.find(user => user.username === username)
+//return type `: User` is put so that when someone refactors the code they will know that we are returning a user obj and not user.username
+function fetchUserDetails(username: string): User {
+    const user = users.find(user => user.username === username);
     if (!user) {
         throw new Error(`User with username ${username} not found`)
-    }
-    return user
+    };
+    //if we use user.username here, there will be error because user.username is a string and not an obj which we declared above `: User` unles we use `: string` or remove `: User` (i.e fetchUserDetails(username: string) {}) this function will let us return user.username
+    return user;
 }
+
